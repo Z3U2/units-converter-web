3bd75333ad99b92962c479954035269400d273ae
Z3U2 <ilyassnasr@gmail.com>
2017-01-16 15:48:07 +0100

    Added Bootstrap, Trying to get hashchange thing to work

--- app.js
+++ app.js
@@ -4,22 +4,51 @@ const $ = require('jquery');
 
 const measures = convertModule.getMeasures();
 
+// Helper function
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+// getting all available measures
 $('#measures').html( () => {
   var html = "";
   for (measure of measures) {
-    html = html + " <option value="+measure+">"+measure+"</option> "
+    html = html + " <li><a href=#"+measure+">"+capitalizeFirstLetter(measure)+"</a></li> "
   };
   return html
 } );
 
-
-$('#measures').change( () => {
-  var selectedMeasure = $('#measures option:selected').text();
-  var units = convertModule.getUnits(selectedMeasure);
-  var html;
-  for (unit of units) {
-    html = html + " <option value="+unit.abbr+">"+unit.singular+"</option> "
-  };
-  $(".units").html(html);
+// chosing right units for given measure
+// $('#measures').change( () => {
+//   var selectedMeasure = $('#measures option:selected').text();
+//   var units = convertModule.getUnits(selectedMeasure);
+//   var html;
+//   for (unit of units) {
+//     html = html + " <option value="+unit.abbr+">"+unit.singular+"</option> "
+//   };
+//   $(".units").html(html);
+//
+// });
+$(window).on('hashchange', () => {
+  const hash = window.location.hash.slice(1);
+  if (hash.length>0) {
+    let units = convertModule.getUnits(hash);
+    var html;
+    for (unit of units) {
+      html = html + " <option value="+unit.abbr+">"+unit.singular+"</option> "
+    };
+    $(".units").html(html)
+  }
 
 });
+
+$('#value').keyup(() => {
+  var value = $('#value').val();
+  console.log(value);
+  value = parseInt(value,10);
+  var fromUnit = $("#from").val();
+  var toUnit = $("#to").val();
+  var convertedValue = convertModule.convertValue(value,fromUnit,toUnit);
+  $("#convertedValue").html(convertedValue);
+  console.log(convertedValue);
+})
